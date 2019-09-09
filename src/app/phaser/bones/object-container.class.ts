05b2ca35f9e5190bb86dd9b189d2d22afaebdd38
CVI <cvimbert@quanticdream.com>
2019-09-09 17:11:19 +0200

    Test commit

--- src/app/phaser/bones/object-container.class.ts
+++ src/app/phaser/bones/object-container.class.ts
@@ -40,6 +40,9 @@ export class ObjectContainer {
         }
         
         this.calculateInitAngleAndHypothenus();
+
+        console.log("ok");
+        
     }
 
     calculateInitAngleAndHypothenus() {
